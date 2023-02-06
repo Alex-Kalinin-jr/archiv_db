a2a847587f8aab07b7da2d92d201e9fd84f5d967
Alexey <alexey@debian.pro>
2023-02-06 23:21:54 +0700

    next to

--- main.cc
+++ main.cc
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
   //    Q_INIT_RESOURCE(application);
   QApplication app(argc, argv);
   QCoreApplication::setOrganizationName("my_org");
-  QCoreApplication::setApplicationName("my_app");
+  QCoreApplication::setApplicationName("База данных");
   QCoreApplication::setApplicationVersion(QT_VERSION_STR);
   QCommandLineParser parser;
   parser.setApplicationDescription(QCoreApplication::applicationName());
@@ -17,9 +17,6 @@ int main(int argc, char *argv[]) {
   parser.addPositionalArgument("file", "the file to open");
   parser.process(app);
   MainWindow w;
-  //    if (!parser.positionalArguments().isEmpty()) {
-  //        w.loadFile(parser.positionalArguments().first());
-  //    }
   w.show();
   return app.exec();
 }
